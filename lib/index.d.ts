63636a838cb6a47943d6ebcd3640b829f2727ce4
Red Forks <redforks@gmail.com>
2019-04-18 10:27:52 +0800

    Add fail() function

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,3 @@
 export declare function assert(test: any, message?: string): void;
+export declare function fail(message?: string): void;
 export declare function notNull<T>(v: T | null | undefined, message?: string): T;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 export function assert(test: any, message?: string) {
-  const prefix = 'Assertion failed';
   if (!test) {
-    throw Error(message ? `${prefix}: ${message}` : prefix);
+    fail(message);
   }
+}
+
+export function fail(message?: string) {
+  const prefix = 'Assertion failed';
+  throw Error(message ? `${prefix}: ${message}` : prefix);
 }
 
 export function notNull<T>(v: T | null | undefined, message?: string): T {
